285b2cf817e31358a9815af1ecc4c8c4d77ed2e4
thiagoeidi <thiagoeidihamada10@hotmail.com>
2025-02-05 01:44:22 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -27,34 +29,57 @@ def test_creat_user(client):
 
 
 def test_read_users(client):
-    response = client.get('/users/')
-
+    response = client.get('/users')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'testusername',
-                'email': 'test@test.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
-            'password': '123',
-            'username': 'testusername2',
-            'email': 'test@test.com',
-            'id': 1,
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
         },
     )
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'username': 'testusername2',
-        'email': 'test@test.com',
+        'username': 'bob',
+        'email': 'bob@example.com',
         'id': 1,
+    }
+
+
+def test_update_integrity_error(client, user):
+    # Criando um registro para "fausto"
+    client.post(
+        '/users',
+        json={
+            'username': 'fausto',
+            'email': 'fausto@example.com',
+            'password': 'secret',
+        },
+    )
+
+    # Alterando o user.username das fixture para fausto
+    response_update = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'fausto',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response_update.status_code == HTTPStatus.CONFLICT
+    assert response_update.json() == {
+        'detail': 'Username or Email already exists'
     }
 
 
@@ -72,21 +97,21 @@ def test_not_found_user_put(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_exerc_get_user_found(client):
+def test_delete_user(client, user):
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_exerc_get_user_found(client, user):
     response = client.get(
         '/users/1',
     )
     assert response.json() == {
-        'username': 'testusername2',
-        'email': 'test@test.com',
+        'username': 'Teste',
+        'email': 'teste@test.com',
         'id': 1,
     }
-
-
-def test_delete_user(client):
-    response = client.delete('/users/1')
-
-    assert response.json() == {'message': 'User deleted'}
 
 
 def test_not_found_user_delete(client):
@@ -97,3 +122,29 @@ def test_not_found_user_delete(client):
 def test_exerc_get_user_not_found(client):
     response = client.get('/users/2')
     assert response.json() == {'detail': 'User not found'}
+
+
+def text_exerc_post_already_exist_username(client, user):
+    response_create = client.post(
+        '/users/',
+        json={
+            'username': 'Teste',
+            'email': 'teste2@test.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response_create.status_code == HTTPStatus.BAD_REQUEST
+    assert response_create.json() == {'detail': 'Username already exists'}
+
+
+def text_exerc_post_already_exist__email(client, user):
+    response_create = client.post(
+        '/users/',
+        json={
+            'username': 'Teste',
+            'email': 'teste@test.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response_create.status_code == HTTPStatus.BAD_REQUEST
+    assert response_create.json() == {'detail': 'Email already exists'}
