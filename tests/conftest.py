4d19dcdd7a1b564cc56669ec445f782162573dca
Thiago Eidi <thiagoeidihamada10@hotmail.com>
2025-03-13 16:54:21 -0300

    erro assync greenlet

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 from contextlib import contextmanager
 from datetime import datetime
 
-import factory
 import pytest
+import pytest_asyncio
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine, event
-from sqlalchemy.orm import Session
+from sqlalchemy import event
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.pool import StaticPool
 
 from fast_zero.app import app
@@ -26,35 +26,36 @@ def client(session):
     app.dependency_overrides.clear()
 
 
-@pytest.fixture
-def session():
-    engine = create_engine(
-        'sqlite:///:memory:',
+@pytest_asyncio.fixture
+async def session():
+    engine = create_async_engine(
+        'sqlite+aiosqlite:///:memory:',
         connect_args={'check_same_thread': False},
         poolclass=StaticPool,
     )
-    table_registry.metadata.create_all(engine)
+    async with engine.begin() as conn:
+        await conn.run_sync(table_registry.metadata.create_all)
 
-    with Session(engine) as session:
+    async with AsyncSession(engine, expire_on_commit=False) as session:
         yield session
 
-    table_registry.metadata.drop_all(engine)
+    async with engine.begin() as conn:
+        await conn.run_sync(table_registry.metadata.drop_all)
 
 
-# HACK: Gerenciador de contexto para criar um padrão de horario de criação de qualquer model que tenha como padrão o campo created_at
 @contextmanager
 def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
-    def fake_time_hook(mapper, connection, target):
+    def fake_time_handler(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
         if hasattr(target, 'updated_at'):
             target.updated_at = time
 
-    event.listen(model, 'before_insert', fake_time_hook)
+    event.listen(model, 'before_insert', fake_time_handler)
 
     yield time
 
-    event.remove(model, 'before_insert', fake_time_hook)
+    event.remove(model, 'before_insert', fake_time_handler)
 
 
 @pytest.fixture
@@ -62,28 +63,17 @@ def mock_db_time():
     return _mock_db_time
 
 
-@pytest.fixture
-def user(session):
+@pytest_asyncio.fixture
+async def user(session):
     password = 'testtest'
-    user = UserFactory(password=get_password_hash(password))
-
+    user = User(
+        username='Teste',
+        email='teste@test.com',
+        password=get_password_hash(password),
+    )
     session.add(user)
-    session.commit()
-    session.refresh(user)
-
-    user.clean_password = password
-
-    return user
-
-
-@pytest.fixture
-def other_user(session):
-    password = 'testtest'
-    user = UserFactory(password=get_password_hash(password))
-
-    session.add(user)
-    session.commit()
-    session.refresh(user)
+    await session.commit()
+    await session.refresh(user)
 
     user.clean_password = password
 
@@ -97,12 +87,3 @@ def token(client, user):
         data={'username': user.email, 'password': user.clean_password},
     )
     return response.json()['access_token']
-
-
-class UserFactory(factory.Factory):
-    class Meta:
-        model = User
-
-    username = factory.Sequence(lambda n: f'test{n}')
-    email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
-    password = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
